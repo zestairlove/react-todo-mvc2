5149726b4e2538d8ce19f8fddc186628fea85e5a
zestairlove <violetdessin@gmail.com>
2019-04-05 17:35:22 +0900

    add change/insert

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,13 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const Header = ({ value }) => {
+const Header = ({ value, onChange, onInsert }) => {
+  const handleKeyDown = e => {
+    if (e.key === 'Enter' && e.target.value !== '') {
+      onInsert()
+    }
+  }
+
   return (
     <header>
       <h1 className={cx('todo__title')}>todos</h1>
@@ -12,6 +18,8 @@ const Header = ({ value }) => {
         type="text"
         className={cx('todo__new-input')}
         value={value}
+        onChange={onChange}
+        onKeyDown={handleKeyDown}
       />
       <div className={cx('todo__toggle-all')}>
         <input id="toggle-all" type="checkbox" />
