30593761d14571e7ac84f5852627fac9f9874426
zestairlove <violetdessin@gmail.com>
2019-07-26 22:10:23 +0900

    add logger

--- src/store.js
+++ src/store.js
@@ -1,14 +1,16 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import reduxThunk from 'redux-thunk';
 
 import modules from './modules';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const logger = createLogger();
 
 // create store
 const store = createStore(
   modules,
-  composeEnhancers(applyMiddleware(reduxThunk))
+  composeEnhancers(applyMiddleware(logger, reduxThunk))
 );
 
 export default store;
